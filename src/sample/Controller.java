4e3168444f5468399ad838e8925c8e4c419f1f35
JulyaDon <don.julya@yandex.ru>
2016-12-08 19:40:54 +0200

    2.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML private TextField BarcodeTextField;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if(BarcodeTextField.getText() == "barcode"){
+            BarcodeTextField.positionCaret(0);
+        }
+    }
+    public void Connect1clicked(ActionEvent actionEvent) {
+    }
+
+    public void Disconnect1clicked(ActionEvent actionEvent) {
+    }
+
+    public void Refresh1clicked(ActionEvent actionEvent) {
+    }
+
+    public void Connect2clicked(ActionEvent actionEvent) {
+    }
+
+    public void Disconnect2clicked(ActionEvent actionEvent) {
+    }
+
+    public void Refresh2clicked(ActionEvent actionEvent) {
+    }
 }
