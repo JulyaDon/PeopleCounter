c683f16e8e42ac53be4f1b5aa0301f126c9563de
JulyaDon <don.julya@yandex.ru>
2016-12-09 20:21:26 +0200

    Magic with controllers.

---
+++ src/controllers/BarcodeController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by July on 09.12.2016.
+ */
+public class BarcodeController {
+    private Controller main;
+
+    public void init(Controller controller) {
+        main = controller;
+
+    }
+
+}
--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,23 +1,35 @@
-package sample;
+package controllers;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML private TextField BarcodeTextField;
 
+    @FXML
+    public void onEnter(ActionEvent ae){
+        BarcodeTextField.setText("");
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        if(BarcodeTextField.getText() == "barcode"){
-            BarcodeTextField.positionCaret(0);
-        }
+
+
     }
+
+
     public void Connect1clicked(ActionEvent actionEvent) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                BarcodeTextField.requestFocus();
+            }
+        });
     }
 
     public void Disconnect1clicked(ActionEvent actionEvent) {
