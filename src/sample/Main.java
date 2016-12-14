32afcbd19a3a43fb3865425c855d15e859f2303b
AndrewDl <dlandrew@ukr.net>
2016-12-14 05:21:51 +0200

    JSON class wrapper

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("People counter");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
